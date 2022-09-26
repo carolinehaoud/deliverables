be571a57ebdde308874ef5ef7fd9ac0bb2127a32
Caroline <67718713+carolinehaoud@users.noreply.github.com>
2022-09-26 19:56:47 -0400

    Update InputForm.jsx
    created InputForm constant

--- week_2/src/components/InputForm.jsx
+++ week_2/src/components/InputForm.jsx
@@ -5,3 +5,22 @@ const SubmitButton= styled.button`
   padding: 0.5 rem 1 rem;
   font-weight: bold;
 `;
+
+const InputForm = () => {
+    return (
+        <Form>
+            <span>Log your Sources:</span>
+            <span>
+                <label for="name">Source Name:</label>
+                <input type="text" id="name" />
+            </span>
+            <span>
+                <label for="email">Source Email:</label>
+                <input type="email" id="email" />
+            </span>
+            <SubmitButton>Submit</SubmitButton>
+        </Form>
+    )
+};
+
+export default InputForm;
