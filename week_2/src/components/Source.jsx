26b33975233db3cd0a4b158ab6ebab28691b243c
Caroline <67718713+carolinehaoud@users.noreply.github.com>
2022-09-26 20:16:24 -0400

    Update Source.jsx
    created the delete button

--- week_2/src/components/Source.jsx
+++ week_2/src/components/Source.jsx
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
+const DeleteButton = styled.button`
+    padding: 0.5rem 1rem;
+    background: ${props => props.color};
+    font-weight: bold;
+`;
+
 export default Source;
