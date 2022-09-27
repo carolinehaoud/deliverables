4f15a5f3c8cb75d788c749d75362c5a9645fca85
Caroline <67718713+carolinehaoud@users.noreply.github.com>
2022-09-26 20:18:07 -0400

    Update Source.jsx
    created row const for react app

--- week_2/src/components/Source.jsx
+++ week_2/src/components/Source.jsx
@@ -6,4 +6,15 @@ const DeleteButton = styled.button`
     font-weight: bold;
 `;
 
+const Row = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 5rem;
+    margin: 1rem 0;
+
+    span {
+        width: 25%;
+    }
+`;
+
 export default Source;
